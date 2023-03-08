3a35b983cc4039f40ae0cd0536026284f1b3a02f
Xaquín Pérez Bermo <xaquin_perez_bermo@outlook.es>
2023-03-08 15:33:59 +0100

    add bottom_nav_menu

---
+++ src/app/src/main/java/com/apm/monsteraltech/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.apm.monsteraltech
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.apm.monsteraltech.R
+import com.apm.monsteraltech.databinding.FragmentMainActivityBinding
+import com.apm.monsteraltech.ui.add.AddFragment
+import com.apm.monsteraltech.ui.fav.FavFragment
+import com.apm.monsteraltech.ui.home.HomeFragment
+import com.apm.monsteraltech.ui.profile.ProfileFragment
+
+class MainActivity : AppCompatActivity() {
+}
