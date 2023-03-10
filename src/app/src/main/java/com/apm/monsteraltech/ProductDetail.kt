df0a0139595498156610ef660f48895817c63fa0
jacobo-fiano1 <72132311+jacobo-fiano1@users.noreply.github.com>
2023-03-10 16:25:45 +0100

    added Product Detail view

---
+++ src/app/src/main/java/com/apm/monsteraltech/ProductDetail.kt
@@ -0,0 +1,11 @@
+package com.apm.monsteraltech
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ProductDetail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_detail)
+    }
+}
