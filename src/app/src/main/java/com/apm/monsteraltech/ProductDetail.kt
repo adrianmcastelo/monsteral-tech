b30dd0bf01bd19aa9ca9ec1deecd7ff67ef71f9e
Xaquín <xaquin.perez1@udc.es>
2023-04-13 23:26:37 +0200

    Añadimos actionbar a productDetail

--- src/app/src/main/java/com/apm/monsteraltech/ProductDetail.kt
+++ src/app/src/main/java/com/apm/monsteraltech/ProductDetail.kt
@@ -1,11 +1,17 @@
 package com.apm.monsteraltech
 
+import android.app.Notification.Action
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.widget.Toolbar
 
-class ProductDetail : AppCompatActivity() {
+class ProductDetail : ActionBarActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_detail)
+        val t: Toolbar = findViewById<View>(R.id.my_toolbar) as Toolbar
+        setSupportActionBar(t)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true);
     }
 }
